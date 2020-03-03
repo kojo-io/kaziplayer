ed5c393afae7a680bc0c81b637c7f5c552664b52
kojo-io <macdee13694@gmail.com>
2020-03-03 20:15:21 +0000

    karma update

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     customLaunchers: {
       ChromeNoSandbox: {
-        base: 'Chrome',
+        base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
     },
